827d9b60100a9d282a03c081ede4b8e6bf444cda
sw333zy <sw333zy@gmail.com>
2019-01-24 11:45:46 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Cost \t \t Quantity \t Total");
+        System.out.print("\n");
+        System.out.print("$1,000.00 \t \t 4 \t\t $4,000.00");
+        System.out.print("\n");
+        System.out.print("$   50.00 \t \t 8  \t   $400.00");
+        System.out.print("\n");
+        System.out.println("-----------------------------------");
+        System.out.print("Total:\t\t\t 12 \t $4,400.00");
+    }
+}
